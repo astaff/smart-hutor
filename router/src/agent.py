0ed551a46e4b9bd8d2470922ab0d677dcd80d748
Maxim Adelman <maxim.adelman@gmail.com>
2011-12-02 12:04:22 -0500

    Changes to server: - added support for queue-per-device (2 for now, generalize later) to support multiple routers; - combined queue polling and clearing into a single function - moved queue URLs into /queue/[dev], to have 1-to-1 match for /dev/[dev] - added error, informational logging - modified app.yaml to add authentication protection to /dev/* to prevent bots/anauthorized access
    Changes to client:
    - only clear the queue if something was retrieved from it
    - support for GPIO mode of operation; default GPIO is via WL520GU WLAN LED
    - parameters for specifying server device, GPIO line, base URL - defaults same as before

--- router/src/agent.py
+++ router/src/agent.py
@@ -1,10 +1,15 @@
-from xbee import ZigBee
+#!/bin/env python
+
+# modifications to baseline:
+# - only clear the queue if something was retrieved from it
+# - support for GPIO mode of operation - default is via WL520GU WLAN LED
+# - parameters for specifying server device, GPIO line, base URL
+
 import time
-import serial
 import os 
 from optparse import OptionParser
 import httplib
-import simplejson as json
+import datetime
 
 # the normal way would be to use pack, but it doesn't work properly on 
 # asus' processor
@@ -17,40 +22,102 @@ def int_to_str(source):
 
 pin_mappings = {0:'D0',1:'D1'}
 
+# default options
+base_url  = "smart-hutor.appspot.com"
+gpio      = "/proc/diag/led/wlan"
+gpio_mode = False
+sdevice   = "device1"
+
 parser = OptionParser()
 parser.add_option("-m", "--modem", dest="filename", 
                   help="Device path XBee is attached to", 
                   metavar="FILE")
+parser.add_option("-u", "--base-url", dest="baseurl", 
+                  help="Base URL for the application, without http:// prefix; default: " + base_url, 
+                  metavar="URL")
+parser.add_option("-l", "--gpio-line", dest="gpio", 
+                  help="File representing GPIO line, default: " + gpio, 
+                  metavar="GPIO")
+parser.add_option("-g", "--gpio-mode", dest="gpio_mode", action="store_true",
+                  help="Run in GPIO mode, default is Zigbee mode")
+parser.add_option("-d", "--server-device", dest="sdevice", 
+                  help="Device queue on the server, default:  " + sdevice)
+
 
 (options, args) = parser.parse_args()
 
-if options.filename is None :
-    print "--modem=PATH is required. Please provide a full device path to the serial port XBee is attached to"
-    os._exit(0)
+if options.baseurl != None:
+    base_url = options.baseurl
 
-ser = serial.Serial(options.filename, 9600)
-time.sleep(0.2)
+if options.gpio != None:
+    gpio = options.gpio
 
-xbee = ZigBee(ser)
+if options.gpio_mode == True:
+    gpio_mode = True
 
+if options.sdevice != None:
+    sdevice = options.sdevice
+
+# conditionally configure for zigbee 
+# hack:  zigbee router has simplejson, xbee and serial modules installed, GPIO router has regular json
+if not gpio_mode:
+    from xbee import ZigBee
+    import simplejson as json
+    import serial
+
+    if options.filename is None :
+        print "--modem=PATH is required. Please provide a full device path to the serial port XBee is attached to"
+        os._exit(0)
+
+        ser = serial.Serial(options.filename, 9600)
+        time.sleep(0.2)
+
+        xbee = ZigBee(ser)
+else:
+    import json
+
+# main loop
 while True :
-    connection = httplib.HTTPConnection("smart-hutor.appspot.com")
-    connection.request("GET", "/queue/all")
+    connection = httplib.HTTPConnection(base_url)
+    connection.request("GET", "/queue/" + sdevice + "/all")
     res = connection.getresponse()
 
     if (res.status == 200):
         s = res.read()
         queue = json.loads(s)
-        connection.request("GET", "/queue/clear")
-        connection.getresponse()
+
+        # only clear queue if messages were retrieved from it
+        if len(queue):
+            connection.request("GET", "/queue/" + sdevice + "/clear")
+            connection.getresponse()
+
+        now = datetime.datetime.now()
         
-        for item in queue:        
-            # addr2 = '\x00\x13\xa2\x00\x40\x3b\xc5\x3a'
-            addr = int_to_str(item[0])
-            xbee.remote_at(dest_addr_long=addr, command=pin_mappings[item[1]], parameter='\x05')
-            time.sleep(0.5)        
-            xbee.remote_at(dest_addr_long=addr, command=pin_mappings[item[1]], parameter='\x04')
-            time.sleep(0.2)
+        for item in queue:      
+
+            # log the action taken
+            if item[1] == 0:
+                print "%s: switching to high" % now
+            else:
+                print "%s: switching to low" % now
+
+            if not gpio_mode:
+                # addr2 = '\x00\x13\xa2\x00\x40\x3b\xc5\x3a'
+                addr = int_to_str(item[0])
+                xbee.remote_at(dest_addr_long=addr, command=pin_mappings[item[1]], parameter='\x05')
+                time.sleep(0.5)        
+                xbee.remote_at(dest_addr_long=addr, command=pin_mappings[item[1]], parameter='\x04')
+                time.sleep(0.2)                
+            else:
+                if item[1] == 0:
+                    ret = os.system("echo 1 > " + gpio)
+                else:
+                    ret = os.system("echo 0 > " + gpio)
+
+                if ret:
+                    print "Error setting the GPIO line"
+
     time.sleep(1)
-    
-ser.close()
+
+if not gpio_mode:    
+    ser.close()
--- server/main.py
+++ server/main.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+import logging
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
@@ -29,8 +32,12 @@ class Command(db.Model):
     pin = db.IntegerProperty()
     name = db.StringProperty()
     
-class Queue(db.Model):
-    command = db.ReferenceProperty(reference_class = Command, collection_name= 'queue_instances')
+class QueueDev1(db.Model):
+    command = db.ReferenceProperty(reference_class = Command, collection_name= 'queue_instances', name = "device1")
+    id = db.IntegerProperty()
+
+class QueueThermWest(db.Model):
+    command = db.ReferenceProperty(reference_class = Command, collection_name= 'queue_instances2', name = "therm_west")
     id = db.IntegerProperty()
 
 class MainHandler(webapp.RequestHandler):
@@ -39,39 +46,86 @@ class MainHandler(webapp.RequestHandler):
 
 class DeviceCommandHandler(webapp.RequestHandler):
     def get(self):
+
         s = self.request.path
         tokens = s.split('/')
+
         if len(tokens) < 4: 
+            logging.error("Unsupported device request " + s)
             self.error(403)
             return 
             
         node = Node.all().filter('name =', tokens[2]).get()
         if node == None :
+            logging.error("Unsupported device " + tokens[2] + " in request " + s)           
             self.error(403)
             return
         
         c = node.commands.filter('name =', tokens[3]).get()
         if c == None :
+            logging.error("Unsupported command " + tokens[3] + " in request " + s)           
             self.error(403)
             return
         
-        Queue(command = c).put()
+        # put command into the right queue for device
+        if node.name == 'device1':
+            q = QueueDev1()
+        else:
+            q = QueueThermWest()
+
+        logging.info("Adding command '%s' to queue for device '%s'" % (tokens[3], tokens[2]));
+
+        q.command = c
+        q.put()
+
 
 class QueueCommandHandler(webapp.RequestHandler):
     def get(self):
         results = []
-        for queue_item in Queue.all().run():
-            results.append((queue_item.command.node.address, queue_item.command.pin))
+
+        s = self.request.path
+        tokens = s.split('/')
+
+        if len(tokens) < 4: 
+            self.error(403)
+            return 
+
+        # identify the queue
+        if tokens[2] == "device1":
+            #logging.info("Queue command handler: dev1")
+            items = QueueDev1.all()
+            q = QueueDev1
+        elif tokens[2] == "therm_west":
+            #logging.info("Queue command handler: therm_west")
+            items = QueueThermWest.all()
+            q = QueueThermWest
+        else:
+            logging.error("Unsupported queue " + tokens[2] + "in request " + s)
+            self.error(403)
+            return
+
+        # process clear
+        if tokens[3] == "all":
+            #logging.info("Queue command handler: all")
+            for queue_item in items:
+                results.append((queue_item.command.node.address, queue_item.command.pin))
+
+        elif tokens[3] == "clear":
+            #logging.info("Queue command handler: clear")
+            db.delete(items)
+            
+        else:
+            logging.error("Unsupported action " + tokens[3] + "in request " + s)
+            self.error(403)
+            return
           
         self.response.out.write(simplejson.dumps(results))
 
-class QueueClearCommandHandler(webapp.RequestHandler):
-    def get(self):
-        db.delete(Queue.all())
         
 class CleanupHandler(webapp.RequestHandler):
     def get(self):
-        db.delete(Queue.all())
+        db.delete(QueueDev1.all())
+        db.delete(QueueThermWest.all())
         db.delete(Command.all())
         db.delete(Node.all())  
 
@@ -82,11 +136,19 @@ class SetupHandler(webapp.RequestHandler):
         Command(node = newNode, pin = 0, name = "on").put()  
         Command(node = newNode, pin = 1, name = "off").put()
 
+        newNode2 = Node(address = int("0x0013a200403bc53b", 0), name = 'therm_west')
+        newNode2.put()
+        Command(node = newNode2, pin = 0, name = "on").put()  
+        Command(node = newNode2, pin = 1, name = "off").put()
+
 def main():   
+    # supported URLs:
+    # /device/[device1|therm_west]/[on|off]
+    # /queue/[device1|therm_west]/[all|clear]
+
     application = webapp.WSGIApplication([
                                           ('/dev/.*', DeviceCommandHandler),
-                                          ('/queue/all', QueueCommandHandler),
-                                          ('/queue/clear', QueueClearCommandHandler),
+                                          ('/queue/.*',  QueueCommandHandler),
                                           ('/cleanup', CleanupHandler),
                                           ('/setup', SetupHandler)
                                           ],
@@ -94,6 +156,11 @@ def main():
  
 #    application = webapp.WSGIApplication([('/', MainHandler)],
 #                                         debug=True)
+    
+    # on startup, re-run cleanup & setup nodes
+    CleanupHandler()
+    SetupHandler()
+
     util.run_wsgi_app(application)
 
 
